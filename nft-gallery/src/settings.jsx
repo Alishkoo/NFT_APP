e2648e39841a4f67eafb9c2b3925cebffa22ba4a
Alishkoo <baisholanov.alibek@gmail.com>
2025-12-12 11:08:49 +0500

    fixed bugs

---
+++ nft-gallery/src/settings.jsx
@@ -0,0 +1,54 @@
+const NFTS = [
+  {
+    tokenId: 1,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/1.json",
+  },
+  {
+    tokenId: 2,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/2.json",
+  },
+  {
+    tokenId: 3,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/3.json",
+  },
+  {
+    tokenId: 4,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/4.json",
+  },
+  {
+    tokenId: 5,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/5.json",
+  },
+  {
+    tokenId: 6,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/6.json",
+  },
+  {
+    tokenId: 7,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/7.json",
+  },
+  {
+    tokenId: 8,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/8.json",
+  },
+  {
+    tokenId: 9,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/9.json",
+  },
+  {
+    tokenId: 10,
+    tokenURI:
+      "https://ipfs.io/ipfs/bafybeieertzmmfm66z6cdue3343ehbg2cb767gzoguk5bjrsm6or2tz4ty/10.json",
+  },
+];
+
+export default NFTS;
